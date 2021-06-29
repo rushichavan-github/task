4b4389308095d7ddce6a8905ff01c3fef7ca0e1c
Rushikesh Chavan <chavanrishi17@gamil.com>
2021-06-29 09:07:59 +0530

    Project uploaded

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 import os
 import environ 
 
@@ -114,3 +115,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
